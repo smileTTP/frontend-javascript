a6aeb69607afbce8fb9dd2f240c55682c2454c88
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-06 21:07:14 +0100

    students table

--- 0x04-TypeScript/task_0/main.ts
+++ 0x04-TypeScript/task_0/main.ts
@@ -1,1 +1,31 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+const student1: Student = {
+    firstName: "Omar",
+    lastName: "Shrif",
+    age: 16,
+    location: "home no.1"
+}
+const student2: Student = {
+    firstName: "Salma",
+    lastName: "Brada",
+    age: 16,
+    location: "home no.2"
+}
+let studentsList: Student[] = [student1, student2];
 
+const table = document.createElement("table");
+
+studentsList.forEach((student: Student) => {
+    const row = table.insertRow();
+
+    const firstNameCell = row.insertCell();
+    const locationCell = row.insertCell();
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+});
