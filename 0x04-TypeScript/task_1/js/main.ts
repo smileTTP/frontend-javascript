6fc5e45a2e78397ec09a675ebec3e27d8eecdd6e
SmileTTP <ibtissamlazraf@gmail.com>
2025-10-11 12:11:15 +0100

    teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,1 +1,16 @@
-
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+};
+const teacher1: Teacher = {
+    firstName: 'Salma',
+    fullTimeEmployee: false,
+    lastName: 'Badi',
+    location: 'Germany',
+    contract: true,
+};
+console.log(teacher1);
